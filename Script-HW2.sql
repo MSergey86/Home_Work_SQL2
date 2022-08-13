729a513ec8a96b887f97919a2b419a1bdaacd12f
MSergey86 <prdx.morozov@mail.ru>
2022-08-13 12:13:15 +0300

    Second commit

--- Script-HW2.sql
+++ Script-HW2.sql
@@ -1,6 +1,6 @@
 CREATE table if not exists Genre (
     id SERIAL primary key,
-    name VARCHAR(60) not NULL
+    name VARCHAR(60) unique not null
 );
 
 CREATE table if not exists Artist (
@@ -17,7 +17,8 @@ CREATE table if not exists Genre_Artist (
 CREATE table if not exists Album (
     id SERIAL primary key,
     name VARCHAR(60) not null,
-    year INTEGER not NULL
+    year INTEGER not null
+    check(year >= 1900 and year <= 3000)
 );
 
 CREATE table if not exists Artist_Album (
@@ -27,15 +28,18 @@ CREATE table if not exists Artist_Album (
 );
 
 CREATE table if not exists Track (
-    id SERIAL primary key references Album(id),
+    id SERIAL primary key,
     name VARCHAR(60) not null,
-    duration TIME not NULL
+    duration INTEGER not null
+    check(duration between 15 and 3600),
+    album_id INTEGER not null references Album(id)
 );
 
 CREATE table if not exists Compilation (
     id SERIAL primary key,
     name VARCHAR(60) not null,
-    year INTEGER not NULL
+    year INTEGER not null
+    check(year >= 1900 and year <= 3000)
 );
 
 CREATE table if not exists Track_Compilation (
@@ -43,3 +47,7 @@ CREATE table if not exists Track_Compilation (
     Compilation_id INTEGER references Compilation(id),
     CONSTRAINT pk_tc primary key (Track_id, Compilation_id)
 );
+
+
+
+    
